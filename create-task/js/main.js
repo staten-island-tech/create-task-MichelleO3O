040b6e87391930c6ae070ad1b20c2fe9a52b4e07
MichelleO3O <113177083+MichelleO3O@users.noreply.github.com>
2023-02-13 10:17:17 -0500

    create 7

--- create-task/js/main.js
+++ create-task/js/main.js
@@ -19,3 +19,63 @@ data.forEach((element) => {
   `
   );
 });
+
+document.querySelector("kor-btn").addEventListener("click", function () {
+  const allCards = document.getElementById("cards");
+  allCards.innerHTML = "";
+  data
+    .filter((e) => e.nationality === "Korean")
+    .forEach((element) => {
+      DOMSelectors.card.insertAdjacentHTML(
+        "beforeend",
+        `
+    <div id="member-cards">
+    <h2 class="names"> ${element.member}</h2>
+    <img src="${element.image}">
+    <p class="age">${element.age}</p>
+    <p class="nationality">${element.nationality}</p>
+    </div>
+  `
+      );
+    });
+});
+
+document.querySelector("cn-btn").addEventListener("click", function () {
+  const allCards = document.getElementById("cards");
+  allCards.innerHTML = "";
+  data
+    .filter((e) => e.nationality === "Chinese")
+    .forEach((element) => {
+      DOMSelectors.card.insertAdjacentHTML(
+        "beforeend",
+        `
+    <div id="member-cards">
+    <h2 class="names"> ${element.member}</h2>
+    <img src="${element.image}">
+    <p class="age">${element.age}</p>
+    <p class="nationality">${element.nationality}</p>
+    </div>
+  `
+      );
+    });
+});
+
+document.querySelector("us-btn").addEventListener("click", function () {
+  const allCards = document.getElementById("cards");
+  allCards.innerHTML = "";
+  data
+    .filter((e) => e.nationality === "American")
+    .forEach((element) => {
+      DOMSelectors.card.insertAdjacentHTML(
+        "beforeend",
+        `
+    <div id="member-cards">
+    <h2 class="names"> ${element.member}</h2>
+    <img src="${element.image}">
+    <p class="age">${element.age}</p>
+    <p class="nationality">${element.nationality}</p>
+    </div>
+  `
+      );
+    });
+});
